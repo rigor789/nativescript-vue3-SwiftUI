72171dd897f1e34e164d948fa1416d347d753fa8
Igor Randjelovic <rigor789@gmail.com>
2022-12-20 17:06:16 +0100

    chore: add SwiftUI demo

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
-import { createApp } from 'nativescript-vue';
-import Home from './components/Home.vue';
+import { createApp, registerElement } from "nativescript-vue";
+import Home from "./components/Home.vue";
+
+import { SwiftUI } from "@nativescript/swift-ui";
+
+registerElement("SwiftUI", () => SwiftUI);
 
 createApp(Home).start();
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,8 +5,12 @@ import {
   onMounted,
   onUnmounted,
   $navigateTo,
+  reactive,
 } from "nativescript-vue";
+import { registerSwiftUI, UIDataDriver } from "@nativescript/swift-ui";
+
 import Details from "./Details.vue";
+import { Color } from "@nativescript/core";
 
 const counter = ref(0);
 const message = computed(() => {
@@ -27,6 +31,37 @@ onUnmounted(() => {
   console.log("unmounted");
   clearInterval(interval);
 });
+
+// @ts-ignore
+declare const CardProvider: any;
+
+interface ISwiftUIData {
+  title?: string;
+  titleColor?: UIColor;
+  desc?: string;
+  descColor?: UIColor;
+  subTitle?: string;
+  subTitleColor?: UIColor;
+  cardBackgroundColor?: UIColor;
+}
+
+registerSwiftUI(
+  "card",
+  (view) => new UIDataDriver(CardProvider.alloc().init(), view)
+);
+
+const swiftUIData: ISwiftUIData = {
+  title: "Hello World!",
+  titleColor: new Color("white").ios,
+  desc: "with NativeScript from Vue!",
+  subTitle: "Heck Yeah!",
+  subTitleColor: new Color('white').ios,
+  cardBackgroundColor: new Color("#65adf1").ios,
+};
+
+const onSwiftUIEvent = (event: any) => {
+  console.log("SWIFTUI EVENT!");
+};
 </script>
 
 <template>
@@ -36,7 +71,7 @@ onUnmounted(() => {
         <Label text="Home" class="font-bold text-lg" />
       </ActionBar>
 
-      <GridLayout rows="*, auto, auto, *" class="px-4">
+      <GridLayout rows="auto, auto, auto, *" class="px-4">
         <Label
           row="1"
           class="text-xl align-middle text-center text-gray-500"
@@ -52,6 +87,13 @@ onUnmounted(() => {
         >
           View Details
         </Button>
+
+        <SwiftUI
+          row="3"
+          swiftId="card"
+          :data="swiftUIData"
+          @swiftUIEvent="onSwiftUIEvent"
+        />
       </GridLayout>
     </Page>
   </Frame>
